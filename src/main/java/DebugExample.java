1c2976808c6d213d66ba417b1b39eca6789ca121
m.smolarz@gmx.net <m.smolarz@gmx.net>
2020-09-06 10:15:16 +0200

    test

---
+++ src/main/java/DebugExample.java
@@ -0,0 +1,24 @@
+public class DebugExample {
+    public static void main(String[] args) {
+        int firstNumber = 11;
+        int secondNumber = 22;
+        System.out.println(firstNumber);
+        System.out.println(secondNumber);
+
+        if ( firstNumber > secondNumber) {
+            subtractAndDisplay(firstNumber, secondNumber);
+        }
+        else {
+            sumAndDisplay(firstNumber, secondNumber);
+        }}
+    private static void sumAndDisplay (int firstNumber, int secondNumber){
+        int result = firstNumber + secondNumber;
+        System.out.println(result);
+    }
+    private static void subtractAndDisplay (int firstNumber, int secondNumber){
+        int result = firstNumber - secondNumber;
+        System.out.println(result);
+    }
+}
+
+
