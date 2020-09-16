7ab87aeeb2953d50b0281bf293f4bf1e667a8017
m.smolarz@gmx.net <m.smolarz@gmx.net>
2020-09-16 12:00:47 +0200

    Application

--- kodilla-intro/src/main/java/ApplicationTest.java
+++
@@ -1,7 +0,0 @@
-public class ApplicationTest {
-    public static void  main(String[] args){
-        Application application = new Application("Adam", 40.5,178);
-        System.out.println (application.name + "  " + application.age + "  " + application.height);
-        application.checkAge();
-    }
-}
