f494854b550865e6628fa037bf7b8f589d53b729
m.smolarz@gmx.net <m.smolarz@gmx.net>
2020-09-16 12:03:18 +0200

    Application

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -9,13 +9,29 @@ public class Application {
         this.height=height;
 
     }
-    public void checkAge() {
-        if (name != null) {
-            if (age > 30 && height > 160) {
-                System.out.println("User is older than 30 and higher then 160cm");
-            } else {
-                System.out.println("User is younger than 30 or lower than 160cm");
+    public String getName() {
+        return this.name;
+    }
+    public double getAge() {
+        return this.age;
+    }
+    public double getHeight() {
+        return this.height;
+    }
+
+
+
+            public static void main (String[]args){
+                Application application = new Application("Adam", 40.5, 178);
+                if (application.getName() != null) {
+                    if (application.getAge() > 30 && application.getHeight() > 160) {
+                        System.out.println("User is older than 30 and higher then 160cm");
+                    } else {
+                        System.out.println("User is younger than 30 or lower than 160cm");
+                    }
+                    System.out.println("Imie: " + application.name +" "+ "Wiek: "+ application.age + " Wysokosc:  " + application.height);
+
+                }
             }
 
         }
-    }}
