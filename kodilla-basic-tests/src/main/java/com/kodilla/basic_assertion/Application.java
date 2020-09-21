8a6b560db96d08a672727338972b38961d27b48b
m.smolarz@gmx.net <m.smolarz@gmx.net>
2020-09-21 16:45:56 +0200

    kodilla-course.kodilla-basic-tests

--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
@@ -1,2 +1,39 @@
-package com.kodilla.basic_assertion;public class Application {
-}
+package com.kodilla.basic_assertion;
+
+public class Application {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();   //tworze obiekt typu Calculator
+        int a = 5;
+        int b = 8;
+
+        int sumResult = calculator.sum(a, b);
+        boolean correct = ResultChecker.assertEquals(13, sumResult);
+        if (correct) {
+            System.out.println("Metoda sum działa poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda sum nie działa poprawnie dla liczb" + a + "i" + b);
+        }
+
+        int subtractResult = calculator.subtract(a, b);
+        boolean subtractCorrect = ResultChecker.assertEquals(-2, subtractResult);
+        if (subtractCorrect) {
+            System.out.println("Metoda subtract działa poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda subtract nie działa poprawnie dla liczb " + a + " i " + b);
+        }
+
+        int squareResult = calculator.square(a);
+        boolean squareCorrect = ResultChecker.assertEquals(26, subtractResult);
+        if (squareCorrect){
+            System.out.println("Metoda square działa poprawnie dla liczb " + a );}
+        else{
+            System.out.println("Metoda square nie działa poprawnie dla liczb " + a );
+        }
+    }
+    }
+
+
+
+
+
+
--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -1,2 +1,17 @@
-package com.kodilla.basic_assertion;public class Calculator {
-}
+package com.kodilla.basic_assertion;
+
+public class Calculator {
+    public int sum(int a, int b){
+        return a+b;
+
+
+    }
+
+public int subtract(int a, int b){    //metoda odejmujaca liczby
+        return a-b;
+    }
+    public int square(int a){       //metoda podnosząca liczbę do kwadratu
+        return a*a;
+    }
+    }
+
