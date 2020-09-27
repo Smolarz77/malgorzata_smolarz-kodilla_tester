70a720662c9d8333a22f99c2ecf25771f17f25a1
m.smolarz@gmx.net <m.smolarz@gmx.net>
2020-09-27 09:41:27 +0200

    Zad.2_6

--- kodilla-intro/src/main/java/com/kodilla/intro/z2_6/Application.java
+++ kodilla-intro/src/main/java/com/kodilla/intro/z2_6/Application.java
@@ -2,7 +2,8 @@ package com.kodilla.intro.z2_6;
 
 public class Application {
     public static void main(String[] args) {
-        User user = new User("Henryk", 43.5, 172);
+        User user = new User("Henryk", 43.5, 172);   //Tworzenie obiektu: typ zmiennej: User, nazwa zmiennej: user
         Validator.validateUser(user);
+
     }
 }
