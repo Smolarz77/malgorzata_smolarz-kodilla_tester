9c854f8f111eb5978cc1262e64215e4ae9176c3c
m.smolarz@gmx.net <m.smolarz@gmx.net>
2020-10-25 17:23:56 +0100

    srednia liczba postow

--- kodilla-stream/src/main/java/com/codilla/stream/AverageAge.java
+++ kodilla-stream/src/main/java/com/codilla/stream/AverageAge.java
@@ -1,2 +1,15 @@
-package com.codilla.stream;public class AverageAge {
+package com.codilla.stream;
+
+
+
+public class AverageAge {
+    public static void main(String[] args) {
+        double avg = UsersRepository.getUsersList()
+                .stream()
+                .map(u -> u.getAge())
+                .mapToInt(n -> n)
+                .average()
+                .getAsDouble();
+        System.out.println(avg);
+    }
 }
--- kodilla-stream/src/main/java/com/codilla/stream/homework/ForumStats.java
+++ kodilla-stream/src/main/java/com/codilla/stream/homework/ForumStats.java
@@ -1,2 +1,29 @@
-package com.codilla.stream.homework;public class ForumStats {
-}
+package com.codilla.stream.homework;
+
+import com.codilla.stream.UsersRepository;
+
+public class ForumStats {
+    public static void main(String[] args) {
+        //W metodzie main uruchom Stream na kolekcji użytkowników zwracanej przez UsersRepository.
+        //Napisz Stream tak, aby obliczył średnią liczbę postów dla użytkowników, których wiek jest >= 40.
+        double average1 = UsersRepository.getUsersList()
+                .stream()
+                .filter(u -> u.getAge() >= 40)
+                .map(u -> u.getNumberOfPosts())
+                .mapToInt(n -> n)
+                .average()
+                .getAsDouble();
+        System.out.println(average1);
+//Napisz kolejny Stream, który obliczy średnią liczbę postów dla użytkowników, których wiek jest < 40.
+        double average2 = UsersRepository.getUsersList()
+                .stream()
+                .filter(u -> u.getAge() < 40)
+               .map(u-> u.getNumberOfPosts())
+                .mapToInt(n -> n)
+                .average()
+                .getAsDouble();
+        System.out.println(average2);
+    }
+
+    }
+
