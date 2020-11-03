f71343ddf3e9df82b789132c5b1edadd54dfd992
m.smolarz@gmx.net <m.smolarz@gmx.net>
2020-11-03 16:51:25 +0100

    7.1

--- kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/homework/UserValidator.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/homework/UserValidator.java
@@ -1,4 +1,22 @@
 package com.kodilla.parametrized_tests.homework;
+import java.util.regex.*;
 
 public class UserValidator {
+    public boolean validateUsername(String username) {
+        //Pierwsza sprawdza, czy przekazana nazwa użytkownika jest prawidłowa:
+        return username.matches("^[a-zA-Z0-9._-]{3,}$");
+    }
+    //Druga metoda sprawdza poprawność przekazanego adresu e-mail:
+
+    public boolean validateEmail(String email) {
+        if (null != email) {
+            String regex = "^([_a-zA-Z0-9-]+(\\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*(\\.[a-zA-Z]{1,6}))?$";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(email);
+            return matcher.matches();
+        }
+        return false;
+
+    }
+
 }
--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/UserValidatorTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/UserValidatorTestSuite.java
@@ -1,6 +1,41 @@
 package com.kodilla.parametrized_tests.homework;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class UserValidatorTestSuite {
+
+    UserValidator userValidator = new UserValidator();
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Gosia", "gosia1", "awTa26._-", "Sa_-.l5m"})
+    public void shouldValidateUsername(String userName) {
+        boolean result = userValidator.validateUsername(userName);
+        assertTrue(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"@@guj", "    ", "$tujk$@", "~//zhz"})
+    public void shouldNotValidateUsername(String userName) {
+        boolean result = userValidator.validateUsername(userName);
+        assertFalse(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Go6@wp.pl", "go9@wp.pl", "GO9@wp.pl", "KAld5@onet.com.pl"})
+    public void shouldValidateEmails(String email) {
+        boolean result = userValidator.validateEmail(email);
+        assertTrue(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Gi#9@gmx.pl", " ", "&%%%%$%^.pl", "mokito"})
+    public void shouldNotValidateEmails(String email) {
+        boolean result = userValidator.validateEmail(email);
+        assertFalse(result);
+    }
+
+
 }
