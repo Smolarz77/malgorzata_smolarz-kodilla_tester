e01ad9260ff267a821c5d9de3a9aac25a96d7132
m.smolarz@gmx.net <m.smolarz@gmx.net>
2020-11-04 12:08:32 +0100

    9.2

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplication.java
@@ -0,0 +1,31 @@
+package com.kodilla.spring.basic.spring_dependency_injection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleApplication {
+
+
+    private SkypeMessageService messageService;
+
+//Adnotacja @Autowired mówi: "znajdź beana o typie określonym poniżej i
+// przypisz go do wskazanej zmiennej".
+
+    //Wstrzykiwanie za pomocą settera
+
+    @Autowired
+    public void setMessageService(SkypeMessageService messageService) {
+        this.messageService = messageService;
+    }
+    public String processMessage(String message, String receiver) {
+        if (checkReceiver(receiver)) {
+            return this.messageService.send(message, receiver);
+        }
+        return null;
+    }
+
+    private boolean checkReceiver(String receiver) {
+        return receiver != null && !receiver.isEmpty();
+    }
+}
