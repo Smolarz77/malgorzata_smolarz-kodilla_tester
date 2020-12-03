5afe808fd192f1573a4344d02d337b5cbd3e0005
Malgorzata Smolarz <m.smolarz@gmx.net>
2020-12-03 17:59:17 +0100

    zad

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -14,8 +14,10 @@ public class AllegroTestingApp {
 
         WebElement alertAccept = driver.findElement(By.xpath("//button[@data-role='accept-consent']"));
         alertAccept.click();
+
         WebElement linkElectronics = driver.findElement(By.xpath("//div[@data-group-id='departments_Elektronika']/a"));
         linkElectronics.click();
+
         WebElement searchItem = driver.findElement(By.xpath("//input[@type='search']"));
         searchItem.sendKeys("Mavic mini");
         searchItem.submit();
