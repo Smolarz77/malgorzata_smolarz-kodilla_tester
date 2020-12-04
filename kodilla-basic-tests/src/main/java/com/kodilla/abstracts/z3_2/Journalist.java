734dcfdc16a06936b7dc52c6c6775a3ef06382b9
Malgorzata Smolarz <m.smolarz@gmx.net>
2020-12-04 09:50:34 +0100

    Zad

--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/z3_2/Journalist.java
+++
@@ -1,7 +0,0 @@
-package com.kodilla.abstracts.z3_2;
-
-public class Journalist extends Job{  //Journalist dziedziczy z Job
-    public Journalist(){
-        super(4000, "Writes articles");
-    }
-}
--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/z3_2/Lawyer.java
+++
@@ -1,8 +0,0 @@
-package com.kodilla.abstracts.z3_2;
-
-public class Lawyer extends Job{   //Lawyer dziedziczy z klasy Job
-    public Lawyer(){
-        super(5000,"obeys the law");
-    }
-
-}
